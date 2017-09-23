238c92abb9b4a66969072db39a71e08d53b742b8
Ashutosh <ashutosh@ultracash.com>
2017-09-23 16:32:57 +0530

    commit simple project

--- hustle-mode/ViewController.swift
+++ hustle-mode/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var powerButton: UIButton!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var onLabel: UILabel!
+    @IBOutlet weak var hustleLabel: UILabel!
+    
+    var player:AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func powerPressed(_ sender: Any) {
+        containerView.isHidden = false
+        player.play()
+
+        UIView.animate(withDuration: 5, animations: {
+            self.rocket.frame = CGRect(x: 0, y: 20, width: 373, height: 173)
+            
+        }) { (finished) in
+            self.onLabel.isHidden = false
+            self.hustleLabel.isHidden = false
+        }
     }
-
-
+    
 }
 
